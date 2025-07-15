799d6ffc6d98d9e83ff5a94604d4144a2eac0330
Hemanth Kumar B <165136617+Hemanthkumar485@users.noreply.github.com>
2025-07-15 22:45:43 +0530

    Time: 1 ms (99.12%), Space: 42 MB (80.13%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -21,3 +21,35 @@ class Solution {
         return vow && con;
     }
 }
+
+// class Solution {
+//         public boolean isValid(String word) {
+//             if(word.length() < 3){
+//                 return false;
+//             }
+//             boolean vow = false;
+//             boolean con = false;
+//             char[] arrvow = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+//             char[] arrcon = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y','z','B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'};
+//             int[] a = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+//             for(int i = 0;i < word.length();i++){
+//                 for (char ch : arrvow){}
+//                     if(word.charAt(i) == ch){
+//                         vow = true;
+//                         break;
+//                     }
+//                 }   
+//                 else if(word.charAt(i) == arrcon){
+//                     con = true;
+//                     break;
+//                 }
+//                 else if(word.charAt(i) == a){
+//                     break;
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//         return vow && con;
+//     }
+// }
