bbef9ef22f02188a601553797de83dd4c4eceba9
Hemanth Kumar B <165136617+Hemanthkumar485@users.noreply.github.com>
2025-07-16 19:24:42 +0530

    Time: 2 ms (100%), Space: 62.3 MB (52.46%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,17 +1,47 @@
 class Solution {
-
     public int maximumLength(int[] nums) {
-        int res = 0;
-        int[][] patterns = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
-        for (int[] pattern : patterns) {
-            int cnt = 0;
-            for (int num : nums) {
-                if (num % 2 == pattern[cnt % 2]) {
-                    cnt++;
-                }
-            }
-            res = Math.max(res, cnt);
+        int[] c = new int[2]; 
+        int[] e = new int[2];   
+
+        for (int num : nums) {
+            int p = num % 2;
+            c[p]++;
+            e[p] = e[1 - p] + 1;
         }
-        return res;
+        return Math.max(
+            Math.max(c[0], c[1]),
+            Math.max(e[0], e[1])
+        );
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+
+//     public int maximumLength(int[] nums) {
+//         int res = 0;
+//         int[][] patterns = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+//         for (int[] pattern : patterns) {
+//             int cnt = 0;
+//             for (int num : nums) {
+//                 if (num % 2 == pattern[cnt % 2]) {
+//                     cnt++;
+//                 }
+//             }
+//             res = Math.max(res, cnt);
+//         }
+//         return res;
+//     }
+// }
