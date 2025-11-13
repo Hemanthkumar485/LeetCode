d1965274da08614987a4e3231ebf123364b2f658
Hemanth Kumar B <165136617+Hemanthkumar485@users.noreply.github.com>
2025-11-13 15:54:26 +0530

    Time: 0 ms (100%), Space: 46.4 MB (10.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,67 @@
+// class Solution {
+//     public int[] twoSum(int[] n, int t) {
+//         int[] a = new int[2];
+//         for(int i = 0;i < n.length;i++){
+//             for(int j = i+1;j < n.length;j++){
+//                 if(n[i]+n[j] == t){
+//                     a[0] = i;
+//                     a[1] = j;
+//                     return a;
+//                 }
+//             }
+//         }
+//         return a;
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] n, int t) {
-        int[] a = new int[2];
-        for(int i = 0;i < n.length;i++){
-            for(int j = i+1;j < n.length;j++){
-                if(n[i]+n[j] == t){
-                    a[0] = i;
-                    a[1] = j;
-                    return a;
+        int num = n.length;
+        for(int i = 1;i < num;i++){
+            for(int j = i;j < num;j++){
+                if(n[j - i] + n[j] == t){
+                    return new int[] {j-i,j};
                 }
             }
         }
-        return a;
+        return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] twoSum(int[] n, int t) {
+//         int arr[]={-1,-1};
+//         for(int i = 0;i < n.length;i++){
+//             for(int j = 0;j < n.length;j++){
+//                 if (i != j){
+//                 if(n[i] + n[j] == t){
+//                     arr[0]=i;
+//                     arr[1]=j;
+//                     return arr;
+//                 }}
+//             }
+//     }
+//     return arr;
+// }}
