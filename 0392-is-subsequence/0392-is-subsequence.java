e6161639a7be6ccb97de95f76f175e8e060e8656
Hemanth Kumar B <165136617+Hemanthkumar485@users.noreply.github.com>
2025-12-05 22:53:11 +0530

    Time: 0 ms (100%), Space: 43 MB (5.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,12 +1,36 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         char[] ch = s.toCharArray();
+//         char[] cc = t.toCharArray();
+//         if(ch.length == 0 && cc.length == 0) return true;
+//         if(cc.length == 0) return false;
+//         Arrays.sort(ch);
+//         Arrays.sort(cc);
+//         boolean b = false;
+//         for(int i = 0;i < ch.length;i++){
+//             if(ch[i] != cc[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        char[] ch = s.toCharArray();
+        char[] cc = t.toCharArray();
+
         int i = 0, j = 0;
-        while (i < s.length() && j < t.length()) {
-            if (s.charAt(i) == t.charAt(j)) {
-                i++; 
+
+        // traverse t with j, match characters of s using i
+        while (i < ch.length && j < cc.length) {
+            if (ch[i] == cc[j]) {
+                i++;
             }
-            j++; 
+            j++;
         }
-        return i == s.length(); 
+
+        return i == ch.length;  // all chars of s matched in order
     }
 }
