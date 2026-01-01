3b9fa1b279d077fbe29717bd671a0af08e65e1db
Hemanth Kumar B <165136617+hemanthkumarb485@users.noreply.github.com>
2026-01-01 14:55:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (58.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -14,3 +14,26 @@ class Solution {
        return arr;
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] plusOne(int[] nums) {
+//         int num = 0;
+//         for(int i : nums) {
+//             num = num * 10 + i;
+//         }
+//         num += 1;
+//         int j = nums.length - 1;
+//         while(num > 0) {
+//             int temp = num % 10;
+//             nums[j] = temp;
+//             num /= 10;
+//         }
+//         return nums;
+//     }
+// }
